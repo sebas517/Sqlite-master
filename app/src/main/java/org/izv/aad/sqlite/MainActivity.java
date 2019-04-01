1a0e11cf9b151a49b5a67baf955fe5e624a79274
dam <dam@iMac-de-DAW-2.local>
2019-04-01 14:20:29 +0200

    Initial commit

---
+++ app/src/main/java/org/izv/aad/sqlite/MainActivity.java
@@ -0,0 +1,15 @@
+package org.izv.aad.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String LOG = "ZZ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
